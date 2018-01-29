b4ebb1f60eb23a3b5c90cafd3c244e18b9666975
Timendum <timedum@gmail.com>
2018-01-29 17:38:58 +0100

    Rewritten for WebExtensions

---
+++ background.js
@@ -0,0 +1,25 @@
+function refreshPac(proxyConfig) {
+    // change proxyType to none
+    const originalUrl = proxyConfig.autoConfigUrl;
+    proxyConfig.autoConfigUrl = 'http://example.com/';
+    browser.browserSettings.proxyConfig.set({value: proxyConfig}).then(function (setResult) {
+        // rollback
+        proxyConfig.autoConfigUrl = originalUrl;
+        if (setResult) {
+            return browser.browserSettings.proxyConfig.set({value: proxyConfig});
+        } else {
+            console.log('Temp browserSettings', setResult);
+        }
+    }).catch(console.log);
+}
+
+browser.browserAction.onClicked.addListener(() => {
+    browser.browserSettings.proxyConfig.get({}).then(function(setting) {
+        const proxyConfig = setting.value;
+        if (proxyConfig.proxyType !== 'autoConfig') {
+            console.log('PAC not enabled in setting');
+        } else {
+            refreshPac(proxyConfig);
+        }
+    });
+});
