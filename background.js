8928674d6a0ff761d5cea4b9a36f80be58c36a34
Timendum <timedum@gmail.com>
2019-02-12 15:54:51 +0100

    Override wrong proxy config

--- background.js
+++ background.js
@@ -1,15 +1,27 @@
 function refreshPac(proxyConfig) {
     // change autoConfigUrl to dummy
+    console.debug("(proxy-reload) Original config: " + JSON.stringify(proxyConfig));
     const originalUrl = proxyConfig.autoConfigUrl;
     proxyConfig.autoConfigUrl = 'http://proxy-reload.firefox.invalid/proxy.pac';
-    browser.proxy.settings.set({value: proxyConfig}).then(function (setResult) {
+    browser.proxy.settings.set({value: proxyConfig}).catch(function(e) {
+        console.error("(proxy-reload) Invalid proxy config found, overwriting (catched: " + e + ")");
+        proxyConfig = {
+            proxyType: 'autoConfig',
+            autoConfigUrl: 'http://example.invalid/',
+            autoLogin: proxyConfig.autoLogin,
+            proxyDNS: proxyConfig.proxyDNS,
+        };
+        return browser.proxy.settings.set({value: proxyConfig});
+    }).then(function (setResult) {
         // rollback
         proxyConfig.autoConfigUrl = originalUrl;
         if (setResult) {
             browser.proxy.settings.set({value: proxyConfig});
         }
-        console.log('Temp browserSettings', setResult);
-    }).catch(console.log);
+        console.log('(proxy-reload) Temp browserSettings', setResult);
+    }).catch((e) => {
+        console.error("(proxy-reload) Error with " + proxyConfig.autoConfigUrl + " - " + e);
+    });
 }
 
 browser.browserAction.onClicked.addListener(() => {
