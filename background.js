9ee020e475e5be3d9e9027460e46734b0f42d2db
Timendum <timedum@gmail.com>
2019-09-16 12:42:24 +0200

    Added a notification to warn about private window access

--- background.js
+++ background.js
@@ -1,3 +1,18 @@
+function checkPrivate() {
+	return browser.extension.isAllowedIncognitoAccess().then(function(answer) {
+		if (answer) {
+			return
+		}
+		browser.notifications.create({
+			"type": "basic",
+			"title": "Reload PAC error",
+			"message": "Reload PAC needs private window access to work.\nThis is a known Firefox limitation.\n\nClick for info.",
+			"iconUrl": browser.runtime.getURL("icon-full.png"),
+		});
+		return Promise.reject('No private window access');
+	})
+}
+
 function refreshPac(proxyConfig) {
     // change autoConfigUrl to dummy
     console.debug("(proxy-reload) Original config: " + JSON.stringify(proxyConfig));
@@ -5,13 +20,13 @@ function refreshPac(proxyConfig) {
     proxyConfig.autoConfigUrl = 'http://proxy-reload.firefox.invalid/proxy.pac';
     browser.proxy.settings.set({value: proxyConfig}).catch(function (e) {
         console.error("(proxy-reload) Invalid proxy config, overwriting (catched: " + e + ")");
-        proxyConfig = {
-            proxyType: 'autoConfig',
-            autoConfigUrl: 'http://example.invalid/',
-            autoLogin: proxyConfig.autoLogin,
-            proxyDNS: proxyConfig.proxyDNS,
-        };
-        return browser.proxy.settings.set({value: proxyConfig});
+		proxyConfig = {
+			proxyType: 'autoConfig',
+			autoConfigUrl: 'http://example.invalid/',
+			autoLogin: proxyConfig.autoLogin,
+			proxyDNS: proxyConfig.proxyDNS
+		};
+		return browser.proxy.settings.set({value: proxyConfig});
     }).then(function (setResult) {
         // rollback
         proxyConfig.autoConfigUrl = originalUrl;
@@ -30,7 +45,13 @@ browser.browserAction.onClicked.addListener(() => {
         if (proxyConfig.proxyType !== 'autoConfig') {
             console.log('PAC not enabled in setting');
         } else {
-            refreshPac(proxyConfig);
+            checkPrivate().then(refreshPac(proxyConfig));
         }
+    });
+});
+
+browser.notifications.onClicked.addListener(function(notificationId) {
+    browser.tabs.create({
+        "url": "https://support.mozilla.org/kb/extensions-private-browsing#w_enabling-or-disabling-extensions-in-private-windows"
     });
 });
